b455ff35e9d819d5c5f3d9239bb070b0c53c4bc0
ChloeJo <zhouqiaohui@hotmail.com>
2022-10-06 12:39:23 -0400

    last working version

--- src/App.js
+++ src/App.js
@@ -4,29 +4,13 @@ import Nav from './components/nav/Nav'
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imagelink/ImageLinkForm';
 import Rank from './components/rank/Rank';
-import Particles from "react-tsparticles";
 import 'tachyons';
 
-const setting = {
-                particles: {
-                  line_linked: {
-                    shadow: {
-                      enable: true,
-                      color: "#3CA9D1",
-                      blur: 5
-                    }
-                  }
-                }
-              }
 export class App extends Component {
   
   render() {
     return (
-      <div className='App'>
-          <Particles 
-                params={setting} 
-            />
-
+      <div className='App'> 
         <Nav />
         <Logo />
         <Rank />
