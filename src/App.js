da54d14b69af27583bc7d735fe7f49734aea0f76
ChloeJo <zhouqiaohui@hotmail.com>
2022-10-07 18:22:51 -0400

    face recognition function works now

--- src/App.js
+++ src/App.js
@@ -10,34 +10,83 @@ import 'tachyons';
 import Clarifai from 'clarifai';
 
 
-const app = new Clarifai.App({
- apiKey: '05bbb7d6de9c43118c0cf7b9ecb86fed'
-});
+// const app = new Clarifai.App({
+//  apiKey: '05bbb7d6de9c43118c0cf7b9ecb86fed'
+// });
+
+const USER_ID = 'j_o_z';
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = '05bbb7d6de9c43118c0cf7b9ecb86fed';
+const APP_ID = 'my-first-application';
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';
+const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
+
 
 export class App extends Component {
   constructor() {
     super();
     this.state = {
       input:'',
-      imageUrl:''
+      imageUrl:'',
+      box: {}
     }
   }
-  
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  } 
+
   onInputChange = (event) => {
     this.setState({input: event.target.value});
-    console.log(this.state.input);
   }
 
   onSubmit = (event) => {
     this.setState({imageUrl:this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL,this.state.input).then(
-        function(response){
-          console.log(response);
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": this.state.imageUrl
+                  }
+              }
+          }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
         },
-        function(err){
-          console.log(err);
-        }
-      );
+        body: raw
+    };
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+        .then(result => result.json())
+        .then(result => this.displayFaceBox(this.calculateFaceLocation(result)))
+        .catch(error => console.log('error', error));
   }
 
   render() {
@@ -51,7 +100,7 @@ export class App extends Component {
               onInputChange={this.onInputChange} 
               onButtonSubmit={this.onSubmit}
             />
-            <FaceRecog imageUrl={this.state.imageUrl}/>
+            <FaceRecog box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
--- src/components/facerecog/FaceRecog.js
+++ src/components/facerecog/FaceRecog.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import './FaceRecog.css';
 
-const FaceRecog = ({imageUrl}) => {
+const FaceRecog = ({imageUrl, box}) => {
     return (
         <div className="center ma">
         	<div className="absolute mt2">
-        		<img src={imageUrl} alt="" width='500px' height='auto'/>
+        		<img id='inputimage' src={imageUrl} alt="" width='500px' height='auto'/>
+        		<div
+        			className="bounding-box" 
+        			style={{top:box.topRow,
+        					right:box.rightCol, 
+        					bottom:box.bottomRow, 
+        					left: box.leftCol}}
+				>
+				</div>
         	</div>
         </div>
     );
